a9721eac53eea4a3d69ab0c7b426065ae91d10fb
lbratkovskaya <lbratkovskaya@naumen.ru>
2021-01-19 22:06:07 +0500

    feat: express routing

--- server/server.js
+++ server/server.js
@@ -5,35 +5,29 @@ const https = require('https');
 const app = express();
 app.use(cors());
 
-// app.get('/api', async (req, res) => {
-    
-//     await https.get(`https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=${req.query.bounds}&faa=1&satellite=1&mlat=1&flarm=1&adsb=1&gnd=0&air=1&vehicles=1&estimated=1&maxage=14400`, (resp) => {
-//         let data = '';
-    
-//         // A chunk of data has been received.
-//         resp.on('data', (chunk) => {
-//             data += chunk;
-//         });
-    
-//         // The whole response has been received. Print out the result.
-//         resp.on('end', () => {
-//             res.send(data);
-//         });
-  
-//     })
-// });
+const apiRouter = express.Router();
 
-app.get('/api/flights', async (req, res) => {
+apiRouter.route('/flights').get( async (req, res) => {
     await https.get(`https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=${req.query.bounds}&faa=1&satellite=1&mlat=1&flarm=1&adsb=1&gnd=0&air=1&vehicles=1&estimated=1&maxage=14400`, responseHandler(res))
 });
 
-app.get('/api/airport', async (req, res) => {
+apiRouter.route('/allAirports').get( async (req, res) => {
+  await https.get('https://www.flightradar24.com/_json/airports.php', responseHandler(res))
+});
+
+apiRouter.route('/airport').get( async (req, res) => {
   await https.get(`https://api.flightradar24.com/common/v1/airport.json?code=${req.query.airportCode}&plugin[]=details&plugin[]=runways&plugin[]=satelliteImage&plugin[]=scheduledRoutesStatistics&plugin[]=weather&plugin-setting[satelliteImage][scale]=1`, responseHandler(res))
 });
 
-app.get('/api/schedule', async (req, res) => {
+apiRouter.route('/schedule').get( async (req, res) => {
   await https.get(`https://api.flightradar24.com/common/v1/airport.json?code=${req.query.airportCode}&plugin[]=schedule&plugin-setting[schedule][mode]=${req.query.mode}&plugin-setting[schedule][timestamp]=${req.query.timestamp}&page=1&limit=100`, responseHandler(res))
 });
+
+apiRouter.route('/flightStatus').get( async (req, res) => {
+  await https.get(`https://data-live.flightradar24.com/clickhandler/?version=1.5&flight=${req.query.flightId}`, responseHandler(res))
+});
+
+app.use('/api', apiRouter);
 
 app.listen(3000, () => {
     console.log('used 3000 port for server');
